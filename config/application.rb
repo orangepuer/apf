90ccd76f6536475490c8ab1f90be5b06aab3a6b6
orangepuer <rkhpuer@gmail.com>
2019-10-02 21:00:00 +0300

    Add gem slim-rails, add config for generate testing specs

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Apf
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+    end
   end
 end
