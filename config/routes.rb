21b398bed83dc9872e21c7a82d2ddb4353fc5fcd
orangepuer <rkhpuer@gmail.com>
2019-10-16 19:15:46 +0300

    Update routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :blogs
   resources :portfolios
 
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
 end
