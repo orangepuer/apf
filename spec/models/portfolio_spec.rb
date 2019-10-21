ca7221115711e9a6afcc8bbbec5b9f8e603b2f45
orangepuer <rkhpuer@gmail.com>
2019-10-21 21:35:05 +0300

    Add validates for blogs, skills, portfolio items

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :main_image }
+  it { should validate_presence_of :thumb_image }
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :percent_utilized }
 end
