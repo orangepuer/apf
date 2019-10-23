4ecc80262d0aa8b4df5ff0ff75e0bffa24ebc9ca
orangepuer <rkhpuer@gmail.com>
2019-10-23 18:11:10 +0300

    Add scopes to Portfolio model, PortfoliosController#react and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     end
   end
   resources :portfolios, except: [:show]
+  get 'react-items', to: 'portfolios#react'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
+3.times do |topic|
+  Topic.create!(title: "Topic #{topic}")
+end
+
 10.times do |blog|
-  Blog.create!(title: "My blog post #{blog}", body: "My Text")
+  Blog.create!(title: "My blog post #{blog}", body: "My Text", topic_id: Topic.last.id)
 end
 
 5.times do |skill|
   Skill.create!(title: "Rails #{skill}", percent_utilized: 15)
 end
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(title: "Portfolio title: #{portfolio_item}",
-                    subtitle: "My great service",
+                    subtitle: "Ruby on Rails",
+                    body: "My Text",
+                    main_image: "https://placehold.it/600x400",
+                    thumb_image: "https://placehold.it/350x200")
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(title: "Portfolio title: #{portfolio_item}",
+                    subtitle: "React",
                     body: "My Text",
                     main_image: "https://placehold.it/600x400",
                     thumb_image: "https://placehold.it/350x200")
