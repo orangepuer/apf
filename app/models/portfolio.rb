c464aa7136271ac6ff37888fbae7eb72d0e4a8db
orangepuer <rkhpuer@gmail.com>
2019-10-24 18:54:37 +0300

    Add callback to Portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
   scope :react, -> { where(subtitle: "React") }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end
 end
