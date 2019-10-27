274f8927afc65a0262de83ad97874cc307483573
orangepuer <rkhpuer@gmail.com>
2019-10-27 22:10:22 +0300

    Add nested attributes for Portfolio model

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Portfolio, type: :model do
   it { should validate_presence_of :body }
   it { should validate_presence_of :main_image }
   it { should validate_presence_of :thumb_image }
+
+  it { should accept_nested_attributes_for :technologies }
 end
