470b637b54880439d5cedd1e94faf9a5032592d9
orangepuer <rkhpuer@gmail.com>
2019-10-28 19:31:09 +0300

    Add nested_attributes to create a portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,6 +14,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.new }
   end
 
   def edit
@@ -55,6 +56,6 @@ class PortfoliosController < ApplicationController
   end
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
