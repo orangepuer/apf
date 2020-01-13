193fd4a4c4305177e1bc9fecde7a928c51a235ef
orangepuer <rkhpuer@gmail.com>
2020-01-13 21:45:51 +0300

    Implement a view helper to dynamically generate a navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,28 @@ module ApplicationHelper
   def copyright_generator message
     ApfViewTool::Renderer.copyright message
   end
+
+  def nav_items
+    [{url: root_path, title: 'Home'}, {url: about_me_path, title: 'About'}, {url: contact_path, title: 'Contact'},
+     {url: blogs_path, title: 'Blog'}, {url: portfolios_path, title: 'Portfolio'}]
+  end
+
+  def nav_helper style, tag_type = '', tag_style = ''
+    nav_link = ''
+
+    nav_items.each do |item|
+      a_tag_str = "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a>"
+      nav_link << "#{add_tag(tag_type, tag_style)}#{a_tag_str}#{add_tag(tag_type, tag_style, '/')}"
+    end
+
+    nav_link.html_safe
+  end
+
+  def add_tag tag_type, tag_style, closing_tag = ''
+    "<#{closing_tag}#{tag_type} class='#{tag_style}'>" if tag_type.present?
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
