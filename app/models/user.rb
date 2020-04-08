9d43e70a5ca3fc4c4b1a6fa6897c50ab56b3a574
orangepuer <rkhpuer@gmail.com>
2020-04-08 21:41:40 +0300

    Add resource Comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :comments, dependent: :destroy
+
   validates_presence_of :name
 
   def first_name
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
   it { should belong_to :topic }
+  it { should have_many :comments }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
