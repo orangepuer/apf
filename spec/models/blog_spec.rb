12df83001ffda51c8f7ecfe85ec5f1bc30356a70
orangepuer <rkhpuer@gmail.com>
2020-04-09 13:15:33 +0300

    Add configure for FactoryBot, update tests

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  let(:blog) { FactoryBot.create(:blog) }
+  let(:blog) { create(:blog) }
 
   it { should belong_to :topic }
   it { expect(blog).to have_many(:comments).dependent(:destroy) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
 
   it { expect(user).to have_many(:comments).dependent(:destroy) }
 
