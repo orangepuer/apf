bc111366ad7a5ba182ab620aeb46e34118893fb5
orangepuer <rkhpuer@gmail.com>
2020-04-10 14:50:55 +0300

    Add form, _comment partial and #create action for comments

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,6 +10,8 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
