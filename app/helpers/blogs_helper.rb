f6c5503abea4112a72a970435726f70bb52f44d2
orangepuer <rkhpuer@gmail.com>
2020-04-13 18:36:53 +0300

    Add gravatar_helper, add styles for comment

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,8 @@
 module BlogsHelper
+  def gravatar_helper user
+    if user
+      hash = Digest::MD5.hexdigest(user.email)
+      image_tag "https://www.gravatar.com/avatar/#{hash}", width: 60
+    end
+  end
 end
